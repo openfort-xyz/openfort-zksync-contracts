e0345429586612a1989ede35f124266c4eaa3df1
Pierre Hay <hay.pierre.pro@gmail.com>
2024-12-10 12:55:30 +0100

    fix [GPR-03M]: enforce bootloader as paymaster caller

--- contracts/paymaster/GeneralPaymaster.sol
+++ contracts/paymaster/GeneralPaymaster.sol
@@ -1,24 +1,28 @@
 pragma solidity ^0.8.0;
 
-import {IPaymaster, ExecutionResult, PAYMASTER_VALIDATION_SUCCESS_MAGIC} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymaster.sol";
+import {
+    IPaymaster,
+    ExecutionResult,
+    PAYMASTER_VALIDATION_SUCCESS_MAGIC
+} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymaster.sol";
 import {IPaymasterFlow} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IPaymasterFlow.sol";
-import {TransactionHelper, Transaction} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
+import {
+    TransactionHelper,
+    Transaction
+} from "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
 
 import {BOOTLOADER_FORMAL_ADDRESS} from "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract GeneralPaymaster is IPaymaster {
     using ECDSA for bytes32;
+
     address public owner;
 
     event NewOwner();
 
     modifier onlyBootloader() {
-        require(
-            msg.sender == BOOTLOADER_FORMAL_ADDRESS,
-            "Only bootloader can call this method"
-        );
-        // Continue execution if called from the bootloader.
+        require(msg.sender == BOOTLOADER_FORMAL_ADDRESS, "Only bootloader can call this method");
         _;
     }
 
@@ -36,30 +40,21 @@ contract GeneralPaymaster is IPaymaster {
         emit NewOwner();
     }
 
-    function validateAndPayForPaymasterTransaction(
-        bytes32,
-        bytes32,
-        Transaction calldata _transaction
-    ) external payable override returns (bytes4 magic, bytes memory context) {
-        magic = PAYMASTER_VALIDATION_SUCCESS_MAGIC;
+    function validateAndPayForPaymasterTransaction(bytes32, bytes32, Transaction calldata _transaction)
+        external
+        payable
+        override
+        onlyBootloader
+        returns (bytes4 magic, bytes memory /* context */ )
+    {
         require(_transaction.paymasterInput.length >= 4, "InvalidInputLength");
-        // TODO: support approval flow with ERC20 sponsoring
+        bytes4 paymasterInputSelector = bytes4(_transaction.paymasterInput[0:4]);
+        require(paymasterInputSelector == IPaymasterFlow.general.selector, "Unsupported paymaster flow");
 
-        bytes4 paymasterInputSelector = bytes4(
-            _transaction.paymasterInput[0:4]
-        );
-
-        if (paymasterInputSelector != IPaymasterFlow.general.selector)
-            revert("Unsupported paymaster flow");
-
-        // Decoding innerInput data to bytes
-        bytes memory signature = abi.decode(
-            _transaction.paymasterInput[4:],
-            (bytes)
-        );
+        bytes memory signature = abi.decode(_transaction.paymasterInput[4:], (bytes));
 
         if (
-           !isValidSignature(
+            isValidSignature(
                 signature,
                 address(uint160(_transaction.from)),
                 address(uint160(_transaction.to)),
@@ -69,17 +64,15 @@ contract GeneralPaymaster is IPaymaster {
                 _transaction.gasLimit
             )
         ) {
-            magic = bytes4(0);
+            magic = PAYMASTER_VALIDATION_SUCCESS_MAGIC;
         }
 
-        uint eth_fee = _transaction.gasLimit * _transaction.maxFeePerGas;
+        uint256 eth_fee = _transaction.gasLimit * _transaction.maxFeePerGas;
         payGas(eth_fee);
     }
 
-    function payGas(uint _eth_fee) internal {
-        (bool success, ) = payable(BOOTLOADER_FORMAL_ADDRESS).call{
-            value: _eth_fee
-        }("");
+    function payGas(uint256 _eth_fee) internal {
+        (bool success,) = payable(BOOTLOADER_FORMAL_ADDRESS).call{value: _eth_fee}("");
         require(success, "gas payment failed");
     }
 
@@ -88,13 +81,13 @@ contract GeneralPaymaster is IPaymaster {
      * @param _transaction The transaction object.
      */
     function postTransaction(
-        bytes calldata /*_context*/,
+        bytes calldata, /*_context*/
         Transaction calldata _transaction,
         bytes32,
         bytes32,
-        ExecutionResult /*_txResult*/,
+        ExecutionResult, /*_txResult*/
         uint256 /*_maxRefundedGas*/
-    ) external payable override {}
+    ) external payable override onlyBootloader {}
 
     function isValidSignature(
         bytes memory _signature,
@@ -105,23 +98,11 @@ contract GeneralPaymaster is IPaymaster {
         uint256 _maxFeePerGas,
         uint256 _gasLimit
     ) public view returns (bool) {
-        bytes32 messageHash = keccak256(
-            abi.encodePacked(
-                _from,
-                _to,
-                _data,
-                _value,
-                _maxFeePerGas,
-                _gasLimit
-            )
-        );
+        bytes32 messageHash = keccak256(abi.encodePacked(_from, _to, _data, _value, _maxFeePerGas, _gasLimit));
 
-        bytes32 ethSignedMessageHash = ECDSA.toEthSignedMessageHash(
-            messageHash
-        );
+        bytes32 ethSignedMessageHash = ECDSA.toEthSignedMessageHash(messageHash);
 
-        (address recoveredAddress, ECDSA.RecoverError error2) = ECDSA
-            .tryRecover(ethSignedMessageHash, _signature);
+        (address recoveredAddress, ECDSA.RecoverError error2) = ECDSA.tryRecover(ethSignedMessageHash, _signature);
         if (error2 != ECDSA.RecoverError.NoError) {
             return false;
         }
